1a4374d698630ebe32aa9110246795758098f3aa
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-28 08:35:54 +0300

    added title and description to the products Table by using Model and migration.
    And added 5 products by using ProductSeeder.

--- laravel/database/seeders/ProductSeeder.php
+++ laravel/database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +15,30 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 1
+        DB::table('products')->insert([
+            'title' => 'HTML Course',
+            'description' => 'This description For HTML Course',
+        ]);
+        // 2
+        DB::table('products')->insert([
+            'title' => 'CSS Course',
+            'description' => 'This description For CSS Course',
+        ]);
+        // 3
+        DB::table('products')->insert([
+            'title' => 'JavaScrip Course',
+            'description' => 'This description For JavaScrip Course',
+        ]);
+        // 4
+        DB::table('products')->insert([
+            'title' => 'PHP Course',
+            'description' => 'This description For PHP Course',
+        ]);
+        // 5
+        DB::table('products')->insert([
+            'title' => 'Laravel Course',
+            'description' => 'This description For Laravel Course',
+        ]);
     }
 }
